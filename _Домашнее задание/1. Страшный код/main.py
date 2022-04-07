107d5a49f4962b8cffd946e28d7305fc2e19d19f
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-07 19:40:42 +0300

    Страшный код

---
+++ _Домашнее задание/1. Страшный код/main.py
@@ -0,0 +1,38 @@
+# a = [1, 5, 3]
+# b = [1, 5, 1, 5]
+# c = [1, 3, 1, 5, 3, 3]
+# for i in b:
+#     a.append(i)
+# t = 0
+# for i in a:
+#     if i == 5:
+#         t += 1
+# print(t)
+# d = []
+# for i in a:
+#     if i != 5:
+#         d.append(i)
+# for i in c:
+#     d.append(i)
+# t = 0
+# for i in d:
+#     if i == 3:
+#         t += 1
+# print(t)
+# print(d)
+#
+
+a = [1, 5, 3]
+b = [1, 5, 1, 5]
+c = [1, 3, 1, 5, 3, 3]
+
+a.extend(b)
+t = a.count(5)
+print('Кол-во цифр 5 при первом объединении:', t)
+for i in range(t):
+    a.remove(5)
+
+a.extend(c)
+t = a.count(3)
+print('Кол-во цифр 3 при втором объединении:', t)
+print('Итоговый список: ', a)
